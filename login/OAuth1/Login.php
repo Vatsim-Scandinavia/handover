96080d05ce2e63593d6e8678e6ab21cf59b96ebe
Daniel <4312175-blt950@users.noreply.gitlab.com>
2019-10-29 20:10:04 +0000

    Added correct encoding of CP1252 characters :blameVATSIM:

--- login/OAuth1/Login.php
+++ login/OAuth1/Login.php
@@ -46,6 +46,17 @@
 
 	// User is already in our database and has accepted, or has just accepted.
 	if(!$error && ($result["acceptedPrivacy"] || isset($_GET["accepted"]) && $_GET["accepted"] == "true")){
+	    
+	    // Due to VATSIM not giving us true UTF-8 names, we need to search and fix the names which are not.
+	    function Windows1252ToUTF8($str){
+            if (preg_match('/Ã€|Ã|Ã‚|Ãƒ|Ã„|Ã…|Ã†|Ã‡|Ãˆ|Ã‰|ÃŠ|Ã‹|ÃŒ|Ã|ÃŽ|Ã|Ã|Ã‘|Ã’|Ã“|Ã”|Ã•|Ã–|Ã—|Ã˜|Ã™|Ãš|Ã›|Ãœ|Ã|Ãž|ÃŸ|Ã|Ã¡|Ã¢|Ã£|Ã¤|Ã¥|Ã¦|Ã§|Ã¨|Ã©|Ãª|Ã«|Ã¬|Ã|Ã®|Ã¯|Ã°|Ã±|Ã²|Ã³|Ã´|Ãµ|Ã¶|Ã·|Ã¸|Ã¹|Ãº|Ã»|Ã¼|Ã½|Ã¾|Ã¿/', $str)){
+                return mb_convert_encoding($str, "Windows-1252", "UTF-8");
+            }
+            return $str;
+        }
+        
+        $vatdata->name_first = Windows1252ToUTF8($vatdata->name_first);
+        $vatdata->name_last = Windows1252ToUTF8($vatdata->name_last);
 
 		// If user exists, update their records. Otherwise create new.
 		if($result){
