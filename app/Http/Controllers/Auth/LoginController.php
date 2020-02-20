99b02382ee71bf2ffe3834bf53c93c877f628f68
Daniel Lange <daniel@vmi282873.contaboserver.net>
2020-02-20 22:34:13 +0100

    Fixed ban error when user not found

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -91,7 +91,7 @@ class LoginController extends Controller
 
                 $user = User::find($sso_data->id);
                 // Check if user is banned
-                if($user->banned){
+                if($user && $user->banned){
                     return redirect()->route('landing')->withError('User '.$user->id.' has been banned in '.env('APP_VACC').' for the following reason: <i>'.$user->banned->reason.'</i><br><br>For inquires contact '.env('APP_VACC_CONTACT').'');
                 }
 
