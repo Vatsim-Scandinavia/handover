487671123564107da6dfe1e19d4c728494511e9d
Matan Budimir <36965687+MatanBudimir@users.noreply.github.com>
2020-09-29 19:31:38 +0200

    Return user data depending on scopes

--- app/Http/Controllers/Api/OAuthUserController.php
+++ app/Http/Controllers/Api/OAuthUserController.php
@@ -5,23 +5,13 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Session;
+use App\Http\Resources\UserCollection;
 
 class OAuthUserController
 {
     public function view(Request $request)
     {
-        $account = $request->user();
-        $return = [];
-
-        $return['id'] = $account->id;
-        $return['first_name'] = $account->first_name;
-        $return['last_name'] = $account->last_name;
-        $return['full_name'] = $account->first_name." ".$account->last_name;
-        $return['email'] = $account->email;
-
-        // Here we can serve more data if we need to do that sometime in future.
-
-        return Response::json(['status' => 'success', 'data' => $return]);
+        return Response::json(['status' => 'success', 'data' => new UserCollection($request->user())]);
     }
 }
 
