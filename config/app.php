5f3472bf49f8e9c2ce7b14b4f973182c07e25f55
blt950 <blt950@users.noreply.github.com>
2021-03-04 22:20:56 +0100

    Made flexible logos

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'VATSCA Handover'),
+    'name' => env('APP_NAME', 'Handover'),
 
     /*
     |--------------------------------------------------------------------------
