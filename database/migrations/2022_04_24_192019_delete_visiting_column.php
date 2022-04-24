39cdee649b2042b9e693b931e043c7afc3246f4d
blt950 <blt950@users.noreply.github.com>
2022-04-24 21:22:48 +0200

    Delete visiting column

---
+++ database/migrations/2022_04_24_192019_delete_visiting_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteVisitingColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('visiting_controller');
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->tinyInteger('visiting_controller')->after('atc_active');
+         });
+    }
+}
