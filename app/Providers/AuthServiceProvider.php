2ad753eaa8223ed7d7e5668734ba4b6611cb5d11
Daniel <blt950@users.noreply.github.com>
2022-12-22 10:55:12 +0100

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,7 +31,7 @@ class AuthServiceProvider extends ServiceProvider
             'full_name' => 'First and last name',
             'email' => 'E-mail address',
             'vatsim_details' => 'Ratings and divisions',
-            'country' => 'Country of residence'
+            'country' => 'Registered country'
         ]);
 
         Passport::setDefaultScope([
